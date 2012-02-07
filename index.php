5723e1c8dd20daaf13c6123184a2900edb1e4306
Eray AYDIN <-_-My_Magic_Life-_-@.(none)>
2012-02-07 17:01:09 +0200

    Initial commit

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
      * then redirect him to the application url 
      * -mahmud
      */
+	 echo  "code kontrol<br>";
     if (isset($_GET['code'])){
         header("Location: " . $fbconfig['appBaseUrl']);
         exit;
@@ -35,6 +36,7 @@
         echo '</pre>';
     }
     // Create our Application instance.
+	echo "facebook nesnesi oluşturma<br>";
     $facebook = new Facebook(array(
       'appId'  => $fbconfig['appid'],
       'secret' => $fbconfig['secret'],
@@ -42,45 +44,47 @@
     ));
 
     //Facebook Authentication part
+	echo "kullanıcı alma <br>";
     $user       = $facebook->getUser();
-	print_r($user);
+	
     // We may or may not have this data based 
     // on whether the user is logged in.
     // If we have a $user id here, it means we know 
     // the user is logged into
     // Facebook, but we don't know if the access token is valid. An access
     // token is invalid if the user logged out of Facebook.
-    
+    echo "giriş adresini alma <br>";
     $loginUrl   = $facebook->getLoginUrl(
             array(
                 'scope'         => 'user_likes'
             )
     );
-	print_r($user);
-   // if ($user) {
-//      try {
-//        // Proceed knowing you have a logged in user who's authenticated.
-//        $user_profile = $facebook->api('/me');
-//		print_r($user_profile);
-//      } catch (FacebookApiException $e) {
-//        //you should use error_log($e); instead of printing the info on browser
-//        d($e);  // d is a debug function defined at the end of this file
-//        $user = null;
-//      }
-//    }
-//
-//    if (!$user) {
+	
+    if ($user) {
+      try {
+        // Proceed knowing you have a logged in user who's authenticated.
+        $user_profile = $facebook->api('/me');
+		print_r($user_profile);
+      } catch (FacebookApiException $e) {
+        //you should use error_log($e); instead of printing the info on browser
+        d($e);  // d is a debug function defined at the end of this file
+        $user = null;
+      }
+    }
+
+   // if (!$user) {
 //        echo "<script type='text/javascript'>top.location.href = '$loginUrl';</script>";
 //        exit;
 //    }
-//    
-//    //get user basic description
-//    $userInfo           = $facebook->api("/$user");
-//	echo "<br><br><br><br><br><br><br><br><br>";
-//	print_r($userInfo);
-//    function d($d){
-//        echo '<pre>';
-//        print_r($d);
-//        echo '</pre>';
-//    }
+    
+    //get user basic description
+    echo "kullanıcı değişkeniyle kullanıcı bilgileri";
+    $userInfo           = $facebook->api("/$user");
+	echo "<br><br><br><br><br><br><br><br><br>";
+	print_r($userInfo);
+    function d($d){
+        echo '<pre>';
+        print_r($d);
+        echo '</pre>';
+    }
 ?>
