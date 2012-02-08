3eb65d08ad50e8adbf50f79c20fa3a9ff3b48ccf
Eray AYDIN <-_-My_Magic_Life-_-@.(none)>
2012-02-08 17:15:24 +0200

    Initial commit

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ stm_em();
 <br />
 <center>
 <font color=#000000 style="font-size:12px">
-Hoşgeldiniz <script language="javascript" type="text/javascript">document.write(self.parent.document.location.href = "document.getElementsByClassName('headerTinymanName')[0]"); </script>, sayfamızı beğenerek bize destek olmayı lütfen unutmayın.</font>
+Hoşgeldiniz <script language="javascript" type="text/javascript">document.write(self.parent.document.location.href = "document.getElementsByClassName('headerTinymanName')[0].innerHTML"); </script>, sayfamızı beğenerek bize destek olmayı lütfen unutmayın.</font>
 </center>
 <div>
 
