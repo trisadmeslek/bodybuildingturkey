28e3fa8c46264caa4b03129e01d97f2a1e972b88
Eray AYDIN <-_-My_Magic_Life-_-@.(none)>
2012-02-08 15:54:39 +0200

    Initial commit

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ body
       'cookie' => true,
     ));
 	$signedRequest = $facebook->getSignedRequest();
-	var_dump($signedRequest["user_id"]);
+	var_dump($signedRequest);
 	echo "<br><br><br><br>";
 	print_r($facebook->api("/sekoyudo"));
 	if($signedRequest["page"]["liked"])
