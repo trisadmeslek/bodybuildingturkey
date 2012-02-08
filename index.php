34706436dbc01c848f5e46100e77f4fd514d1c43
Eray AYDIN <-_-My_Magic_Life-_-@.(none)>
2012-02-08 17:33:33 +0200

    Initial commit

--- index.php
+++ index.php
@@ -13,6 +13,37 @@ body
 </head>
 <body>
 <?php
+
+function parse_signed_request($signed_request, $secret) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+    error_log('Unknown algorithm. Expected HMAC-SHA256');
+    return null;
+  }
+
+  // check sig
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+
+
+$deneme = parse_signed_request($_REQUEST['signed_request'],"fe29febdf7bc6460cb23261050c2a2a8");
+echo $_REQUEST['signed_request'];
+print_r($deneme);
     //facebook application configuration -mahmud
     $fbconfig['appid' ] = "342994075723691";
     $fbconfig['secret'] = "fe29febdf7bc6460cb23261050c2a2a8";
