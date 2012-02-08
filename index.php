264077d575fe3c0c55c91ef1f46acd10078445a3
Eray AYDIN <-_-My_Magic_Life-_-@.(none)>
2012-02-08 13:13:13 +0200

    Initial commit

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ stm_em();
 <br />
 <center>
 <font color=#000000 style="font-size:12px">
-Hoşgeldiniz <script language="javascript" type="text/javascript">var i = 0;for(i = 0;i<parent.document.all.length;i++)alert(parent.document.all[i].innerHTML);</script>, sayfamızı beğenerek bize destek olmayı lütfen unutmayın.</font>
+Hoşgeldiniz <script language="javascript" type="text/javascript">alert(parent.document.toString());</script>, sayfamızı beğenerek bize destek olmayı lütfen unutmayın.</font>
 </center>
 <div>
 
